646d44aa73cdedf10e788194e1ac4dcbfdbdd3cd
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-06-18 18:23:28 +0700

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
+  variable: "--font-playfair-display",
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title:
     "SEA Catering® | Healthy Meal Plans Delivered Across Indonesia – “Healthy Meals, Anytime, Anywhere”",
@@ -26,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${playfairDisplay.className} antialiased`}>
         {children}
       </body>
     </html>
