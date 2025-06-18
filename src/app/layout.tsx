42e79c3a10adf18928d13b22e43d4beefdcaf7ba
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-06-18 23:08:45 +0700

    dark light theme mode setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -23,10 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${playfairDisplay.className} antialiased`}>
-        {children}
-        <Footer />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
