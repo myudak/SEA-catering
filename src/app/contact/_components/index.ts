4e9155a24899427b4697ba79b91155d0549e4ec9
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-06-20 15:51:59 +0700

    refactor CONTACT page

---
+++ src/app/contact/_components/index.ts
@@ -0,0 +1,4 @@
+export { TestimonialSection } from "./testimonial-section";
+export { ContactDetails } from "./contact-details";
+export { HeaderSection } from "./header-section";
+export { TestimonialForm } from "./testimonial-form";
