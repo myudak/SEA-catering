4ade90a258ce9a3fb959bceb7efb14fa9078aa65
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-06-24 19:26:54 +0700

    add utility for getInitial name for avatar fallback string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getInitials(name: string, lowercase = true): string {
+  if (!name) return "";
+  const initials = name
+    .split(" ")
+    .filter(Boolean)
+    .slice(0, 2)
+    .map((word) => word[0])
+    .join("");
+
+  return lowercase ? initials.toLowerCase() : initials.toUpperCase();
 }
