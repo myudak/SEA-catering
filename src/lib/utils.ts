830d153e2d9010638e100034dae3e39fb8da7db4
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-06-27 01:26:04 +0700

    utility for checking admin user supabase

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
+import { SupabaseClient } from "@supabase/supabase-js";
 import { clsx, type ClassValue } from "clsx";
+import { NextResponse } from "next/server";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -15,4 +17,40 @@ export function getInitials(name: string, lowercase = true): string {
     .join("");
 
   return lowercase ? initials.toLowerCase() : initials.toUpperCase();
+}
+
+type Mode = "api" | "action";
+
+export async function checkIsAdmin(
+  supabase: SupabaseClient,
+  mode: Mode = "api"
+) {
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError || !user) {
+    if (mode === "api") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    } else {
+      throw new Error("Unauthorized");
+    }
+  }
+
+  const { data: profile, error: profileError } = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("user_id", user.id)
+    .single();
+
+  if (profileError || !profile || profile.role !== "admin") {
+    if (mode === "api") {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    } else {
+      throw new Error("Forbidden");
+    }
+  }
+
+  return { user, profile };
 }
